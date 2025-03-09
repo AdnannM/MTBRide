adb1e97a34ba489acb5ae7d10bd24b4aea2ee58a
AdnannM <adnannmuratovic@icloud.com>
2025-03-09 17:07:22 +0100

    Initial Commit

---
+++ MTBRide/MTBRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTBRideApp.swift
+//  MTBRide
+//
+//  Created by Adnann Muratovic on 09.03.25.
+//
+
+import SwiftUI
+
+@main
+struct MTBRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
