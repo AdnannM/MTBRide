377292fec34fe86e054c62c43a481eeca93f7fca
AdnannM <adnannmuratovic@icloud.com>
2025-03-11 22:06:32 +0100

    feat setup firebase

---
+++ MTBRide/View/Auth/SignIn.swift
@@ -0,0 +1,22 @@
+//
+//  SignIn.swift
+//  MTBRide
+//
+//  Created by Adnann Muratovic on 11.03.25.
+//
+
+import SwiftUI
+
+struct SignIn: View {
+    var body: some View {
+        VStack {
+            Text("Adannn was here...")
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+        
+}
+
+#Preview {
+    SignIn()
+}
